778a8a48b2d1b93d225fb12586ff88821b3d572c
Minna Sarakontu <minna.sarakontu@gmail.com>
2019-05-12 10:17:14 +0300

    steps 2-5

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
 
 let persons = [
   {
@@ -28,12 +31,42 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
+app.get('/info', (req, res) => {
+  const count = persons.length
+  const timestamp = new Date()
+  res.send(`Puhelinluettelossa on ${count} henkilön tiedot<br />${timestamp}`)
+})
+
 app.get('/api', (req, res) => {
   res.send('<h1>API</h1>')
 })
   
 app.get('/api/persons', (req, res) => {
   res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) res.json(person)
+  else res.status(404).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  const newPerson = {
+    id: Math.floor(Math.random() * 9999) + 5,
+    name: body.name,
+    number: body.number
+  }
+  persons = persons.concat(newPerson)
+  res.json(newPerson)
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
 })
 
 const port = 3001
